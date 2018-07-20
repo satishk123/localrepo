a2ed6d86989a0e74ccef0443244972052040728f
satish <mirafra@mirafra-OptiPlex-780.(none)>
2018-07-20 11:04:35 +0530

    updating

---
+++ samplefile.c
@@ -0,0 +1,20 @@
+
+         #include<stdio.h>
+
+         #include<string.h>
+
+
+int main()
+{
+      char str[]="saisatish";
+      char str2[]="mirafra";
+
+    char str3[60];
+
+
+   strcpy(str3,str);
+   strcat(str3,str2);
+
+  puts(str3);
+}
+
